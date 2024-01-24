8f5dfddce74d1384721eb5316b8d1084fe89bd98
JJ46180 <148750239+JJ46180@users.noreply.github.com>
2024-01-24 11:34:44 +0900

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+});
+//end
